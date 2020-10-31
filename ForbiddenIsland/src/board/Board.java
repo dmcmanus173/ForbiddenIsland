1123c14bfe1fec24e12e5dccac40303cb91fb19e
Demi Oke <demioke98@gmail.com>
2020-10-31 19:48:36 +0000

    tidied up board obj

--- ForbiddenIsland/src/board/Board.java
+++ ForbiddenIsland/src/board/Board.java
@@ -2,10 +2,21 @@ package board;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 import enums.TileEnum;
 import enums.TreasureEnum;
+
+
+/**
+ * Class representing the board object.
+ * @author Demi Oke & Daniel McManus
+ * @date    31/10/2020
+ * @version 0.1
+ *
+ */
 
 
 public class Board {
@@ -16,6 +27,7 @@ public class Board {
     //===========================================================
 	private static Board board;
 	private ArrayList<ArrayList<Optional<Tile>>> islandTiles;
+	private Map<TileEnum, int[]> islandTilesNamePositionMap;
 	private final int NUM_ROWS = 6;
 	private final int NUM_COLS = 6;
 	
@@ -43,9 +55,11 @@ public class Board {
 	 */
     private Board() {
     	ArrayList<Tile> instanciatedTiles = instanciateIslandTiles(); // Instantiate all tiles
-    	
+    	Optional<Tile> islandTile; // dependency
     	// Island tiles are optional as some array positions are empty due to the odd shape of the board
     	this.islandTiles = new ArrayList<ArrayList<Optional<Tile>>>(); 
+    	this.islandTilesNamePositionMap = new HashMap<TileEnum, int[]>();
+    	
     	
     	System.out.println("making board.");
     	for (int y=0; y<NUM_ROWS; y++) {         // For each row in Board
@@ -53,9 +67,11 @@ public class Board {
     		islandTiles.add(new ArrayList<Optional<Tile>>());
             
             for (int x=0; x<NUM_COLS; x++) {     // For each column in row
-            	if (canPlaceTileAtPosition(x, y)) {
-            		Optional<Tile> islandTile = Optional.of(instanciatedTiles.remove(0));
+            	if (tilePositionIsWithinIslandShape(x, y)) {
+            		islandTile = Optional.of(instanciatedTiles.remove(0));
             		islandTiles.get(y).add(islandTile);
+            		int[] pos = {x, y};
+            		islandTilesNamePositionMap.put(islandTile.get().tileName, pos);
             	} else {
             		islandTiles.get(y).add(Optional.empty());
             	}
@@ -65,12 +81,58 @@ public class Board {
     
     
     /**
-     * canPlaceTileAtPosition. Checks if a tile can be placed at a given position on the board
+     * returns a particular board tile.
+     * @param pos the tiles position
+     * @return the tile for a particular position.
+     */
+    private Optional<Tile> getTileAtPosition(int[] pos) {
+    	if(tilePositionIsWithinIslandShape(pos[0], pos[1])) {
+    		return islandTiles.get(pos[1]).get(pos[0]);
+    	} else { 
+    		return Optional.empty();
+    	}
+    }
+    
+    /**
+     * returns a particular board tile.
+     * Unlike getTileAtPosition, this function should ALWAYS return a tile, 
+     * as every island tile should be within the shape of the island.
+     * @param tileName the name of the tile.
+     * @return the tile for a of a given name.
+     * @throws Exception 
+     */
+    private Tile getTileWithName(TileEnum tileName) throws Exception {
+    	int[] pos = islandTilesNamePositionMap.get(tileName);
+        Optional<Tile> islandTile = getTileAtPosition(pos);
+        if (islandTile.isPresent()) {
+        	return islandTile.get();
+        } else {
+        	// the position of every island tile must exist within the island shape.
+        	throw new Exception("The position of the tile with the name: " + tileName + " is not in the feasibile region.");
+        }
+    }
+    
+    
+    /**
+     * returns a particular board tile.
+     * @param pos the tiles position
+     * @return the tile for a particular position.
+     */
+    private Optional<Tile> get(int[] pos) {
+        return islandTiles.get(pos[1]).get(pos[0]);
+    }
+    
+    
+    
+    
+    /**
+     * tileExistsAtPosition. Defines the feasible region where island tiles can exist. 
+     * Checks if a tile can be placed at a given position on the board
      * @param x the x position on the board
      * @param y the y position on the board
      * @return boolean determining if the tile can be placed at the given position.
      */
-    private boolean canPlaceTileAtPosition(int x, int y) {
+    private boolean tilePositionIsWithinIslandShape(int x, int y) {
     	if (y== 0 || y== 5) {
     		if(x>1 && x <4) return true;
     	}
@@ -114,17 +176,45 @@ public class Board {
     }
     
     
+//    public String getTileString(String tileAbbr) {
+//    	StringBuilder tileString = new StringBuilder("");
+//    	String horizLine = "______";
+//    	String vertLine = "|";
+//    	String sixSpaces = "      ";
+//    	String twoSpaces = "  ";
+//    	String oneSpace = " ";
+//    	String zeroStr = " ______";
+//    	System.out.println(tileString.toString());
+//    	tileString.append(" ______ ______");
+//    	tileString.append("|      |      |");
+//    	tileString.append("|  FL  |");
+//    	tileString.append("|______|");
+//    	
+//    	for (int y=0; y<=18; y++) {         // For each row in Board
+//    		
+//            for (int x=0; x<=4; x++) {     // For each column in row
+//            	
+//            }
+//            System.out.println(tileString.toString());
+//            
+//        }
+//
+//    }
+    
+    
+    
+    
     /**
      * instanciateIslandTiles instantiates individual island tiles and shuffle them.
      * @return tiles a shuffled list of all the island tiles in the game.
      */
 	private ArrayList<Tile> instanciateIslandTiles() {
 		ArrayList<Tile> tiles = new ArrayList<Tile>();
+		Optional<TreasureEnum> treasure;
 		for (TileEnum tileName : TileEnum.values()) { 
-			Optional<TreasureEnum> treasure = getAssociatedTreasureForTile(tileName);
-			if (treasure.isPresent()) {
-				TreasureEnum presentTreasure = treasure.get();
-				tiles.add(new TreasureTile(tileName, presentTreasure));
+			treasure = getAssociatedTreasureForTile(tileName);
+			if ( treasure.isPresent() ) {
+				tiles.add(new TreasureTile(tileName, treasure.get()));
 			} else {
 				tiles.add(new Tile(tileName));
 			}
@@ -137,20 +227,21 @@ public class Board {
 	
 	/**
      * getAssociatedTreasureForTile gets the treasureType associated with a given island tile.
+     *  @param tileName the name of the tile.
      * @return Optional TreasureEnum as not all island tiles have an associated treasure.
      */
 	private Optional<TreasureEnum> getAssociatedTreasureForTile(TileEnum tileName) {
-		if (tileName == TileEnum.TEMPLE_OF_THE_MOON || tileName == TileEnum.TEMPLE_OF_THE_SUN) {
+		if (tileName == TileEnum.TEMPLE_OF_THE_MOON || tileName == TileEnum.TEMPLE_OF_THE_SUN) 
 			return Optional.of(TreasureEnum.THE_EARTH_STONE);
-		} else if (tileName == TileEnum.WHISPERING_GARDEN || tileName == TileEnum.HOWLING_GARDEN) {
+		else if (tileName == TileEnum.WHISPERING_GARDEN || tileName == TileEnum.HOWLING_GARDEN)
 			return Optional.of(TreasureEnum.THE_STATUE_OF_WIND);
-		} else if (tileName == TileEnum.CAVE_OF_EMBERS || tileName == TileEnum.CAVE_OF_SHADOWS) {
+		else if (tileName == TileEnum.CAVE_OF_EMBERS || tileName == TileEnum.CAVE_OF_SHADOWS) 
 			return Optional.of(TreasureEnum.THE_CRYSTAL_OF_FIRE);
-		} else if (tileName == TileEnum.CORAL_PALACE || tileName == TileEnum.TIDAL_PALACE) {
+		else if (tileName == TileEnum.CORAL_PALACE || tileName == TileEnum.TIDAL_PALACE) 
 			return Optional.of(TreasureEnum.THE_OCEANS_CHALICE);
-		} else {
+		else 
 			return Optional.empty();
-		}
+		
 		
 	} 
 	
--- ForbiddenIsland/src/enums/TileEnum.java
+++ ForbiddenIsland/src/enums/TileEnum.java
@@ -2,50 +2,54 @@ package enums;
 
 /**
  * Class holding the tile names in the Forbidden Island Game.
- * @author Demi Oke
- * @version 1.0
+ * @author Demi Oke & Daniel McManus
+ * @date    31/10/2020
+ * @version 0.1
  *
  */
 
 public enum TileEnum {
-	FOOLS_LANDING        ("Fools Landing"),
+	FOOLS_LANDING        ("Fools Landing", "FL"),
 	
-	TEMPLE_OF_THE_MOON   ("Temple of the Moon"),
-	TEMPLE_OF_THE_SUN    ("Temple of the Sun"),
-	WHISPERING_GARDEN    ("Whispering Garden"),
-	HOWLING_GARDEN       ("Howling Garden"),
-	CAVE_OF_EMBERS       ("Cave of Embers"),
-	CAVE_OF_SHADOWS      ("Cave of Shadows"),
-	CORAL_PALACE         ("Coral Palace"),
-	TIDAL_PALACE         ("Tidal Palace"),
+	TEMPLE_OF_THE_MOON   ("Temple of the Moon", "TM"),
+	TEMPLE_OF_THE_SUN    ("Temple of the Sun", "TS"),
+	WHISPERING_GARDEN    ("Whispering Garden", "WG"),
+	HOWLING_GARDEN       ("Howling Garden", "HG"),
+	CAVE_OF_EMBERS       ("Cave of Embers", "CE"),
+	CAVE_OF_SHADOWS      ("Cave of Shadows", "CS"),
+	CORAL_PALACE         ("Coral Palace", "CP"),
+	TIDAL_PALACE         ("Tidal Palace", "TP"),
 	
-	BRONZE_GATE          ("Bronze Gate"),
-	SILVER_GATE          ("Silver Gate"),
-	COPPER_GATE          ("Copper Gate"),
-	GOLDEN_GATE          ("Golden Gate"),
-	IRON_GATE            ("Iron Gate"),
-	TWILIGHT_HOLLOW      ("Twilight Hollow"),
-	LOST_LAGOON          ("Lost Lagoon"),
-	CRIMSON_FOREST       ("Crimson Forest"),
-	WATCHTOWER           ("Watchtower"),
-	MISTY_MARSH          ("Misty Marsh"),
-	CLIFFS_OF_ABANDON    ("Cliffs of Abandon"),
-	BREAKERS_BRIDGE      ("Breakers Brisge"),
-	OBSERVATORY          ("Observatory"),
-	PHANTOM_ROCK         ("Phantom Rock"),
-	DUNES_OF_DECEPTION   ("Dunes of Deception");
+	BRONZE_GATE          ("Bronze Gate", "BG"),
+	SILVER_GATE          ("Silver Gate", "SG"),
+	COPPER_GATE          ("Copper Gate", "CG"),
+	GOLDEN_GATE          ("Golden Gate", "GG"),
+	IRON_GATE            ("Iron Gate", "IG"),
+	TWILIGHT_HOLLOW      ("Twilight Hollow", "TH"),
+	LOST_LAGOON          ("Lost Lagoon", "LL"),
+	CRIMSON_FOREST       ("Crimson Forest", "CF"),
+	WATCHTOWER           ("Watchtower", "WT"),
+	MISTY_MARSH          ("Misty Marsh", "MM"),
+	CLIFFS_OF_ABANDON    ("Cliffs of Abandon", "CA"),
+	BREAKERS_BRIDGE      ("Breakers Brisge", "BB"),
+	OBSERVATORY          ("Observatory", "OB"),
+	PHANTOM_ROCK         ("Phantom Rock", "PR"),
+	DUNES_OF_DECEPTION   ("Dunes of Deception", "DD");
 
 	
 	private final String name;
+	private final String abbr;
 	
 	
 	/**
      * constructor for Tiles taking in the name associated 
      * with the tile that will be printed on the board.
      * @param str String which will be set to the name of the tile
+     * @param abbr String which will be set to abbreviation of the name of the tile
      */
-	private TileEnum(String str) {
-		name = str; 
+	private TileEnum(String str, String abbr) {
+		name = str;
+		this.abbr = abbr;
     }
 	
 	
@@ -57,6 +61,17 @@ public enum TileEnum {
 	public String toString() {
 		return this.name;
 	}
+	
+	
+	/**
+    * gets name relating to tile
+    * @return the name.
+    */
+	public String getAbbr() {
+		return this.abbr;
+	}
+	
+	
 	
 	/*
 	// Class-level test
