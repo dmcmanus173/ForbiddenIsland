48faeb79af0ca1c68782629a9c35aff90d35aa08
dmcmanus173 <danielmcmanus173@gmail.com>
2020-11-02 15:08:59 +0000

    Forgot to add comments

--- ForbiddenIsland/src/player/Players.java
+++ ForbiddenIsland/src/player/Players.java
@@ -3,19 +3,29 @@ package player;
 import java.util.ArrayList;
 
 import enums.AdventurerEnum;
-import gameComponents.TreasureDeck;
 
+/**
+ * Class containing all Players in a game of Forbidden Island.
+ * 
+ * @author  Demi Oke and Daniel McManus
+ * @date    02/11/2020
+ * @version 0.1
+ */
 public class Players {
+	
+	//===========================================================
+	// Variable Setup
+	//===========================================================
+	private static Players players;
 	private ArrayList<Player> listPlayers = new ArrayList<Player>();
 	private int numPlayers;
 	
-	private static Players players;
 	//===========================================================
     // Get Instance of Singleton
     //===========================================================
     /**
-     * getInstance method returns single instance of TreasureDeck
-     * @return treasureDeck. singleton TreasureDeck object.
+     * getInstance method returns single instance of Players
+     * @return Players. singleton Players object.
      */
     public static Players getInstance(){
         if(players == null)
@@ -23,23 +33,54 @@ public class Players {
         return players;
     }
     
+	//===========================================================
+	// Constructor
+	//===========================================================
+	/**
+	 * Constructor for Players object.
+	 * At this point, no players are added. numPlayers set to 0.
+	 */
     private Players() {
     	numPlayers = 0;
     }
     
+	/**
+	 * addPlayers will create and add a new player to the listPlayers.
+	 * @param String name, the name of the player.
+	 * @param AdventurerEnum role, role of the player.
+	 */
     public void addPlayer(String name, AdventurerEnum role) {
     	listPlayers.add(new Player(name, role));
     	numPlayers++;
     }
-    
-    public void removeAllPlayers() {
-    	listPlayers.removeAll(listPlayers);
+	/**
+	 * addPlayers will add a new player to the listPlayers.
+	 * @param Player player. Player to be added to listPlayers.
+	 */
+    public void addPlayer(Player player) {
+    	listPlayers.add(player);
+    	numPlayers++;
     }
     
+	/**
+	 * removeAllPlayers will remove all players from the listPlayers
+	 */
+    public void removeAllPlayers() {
+    	listPlayers.removeAll(listPlayers);
+    	numPlayers = 0;
+    }
+    
+	/**
+	 * getPlayers will retrieve Player i.
+	 * @param int i, i is the player number. Not the position of the player in listPlayer. i = pos+1.
+	 */  
     public Player getPlayer(int i) {
     	return listPlayers.get(i-1);
     }
     
+	/**
+	 * toString will return list of players, with respect to Player number.
+	 */
     @Override
     public String toString() {
     	StringBuilder temp = new StringBuilder("");
