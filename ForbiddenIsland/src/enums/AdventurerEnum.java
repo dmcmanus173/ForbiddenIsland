65df22aac4cf0f6e6e9c4d446c79019e81a4f864
Demi Oke <demioke98@gmail.com>
2020-11-06 13:31:38 +0000

    added starting positions to AdventurerEnum

--- ForbiddenIsland/src/enums/AdventurerEnum.java
+++ ForbiddenIsland/src/enums/AdventurerEnum.java
@@ -10,22 +10,24 @@ package enums;
 
 public enum AdventurerEnum {
 	
-	NAVIGATOR  ("Navigator"),
-	DIVER      ("Diver"),
-	MESSENGER  ("Messenger"),
-	EXPLORER   ("Explorer"),
-	PILOT      ("Pilot"),
-	ENGINEER   ("Engineer");
+	NAVIGATOR  ("Navigator", TileEnum.GOLDEN_GATE),
+	DIVER      ("Diver",     TileEnum.IRON_GATE),
+	MESSENGER  ("Messenger", TileEnum.SILVER_GATE),
+	EXPLORER   ("Explorer",  TileEnum.COPPER_GATE),
+	PILOT      ("Pilot",     TileEnum.FOOLS_LANDING),
+	ENGINEER   ("Engineer",  TileEnum.BRONZE_GATE);
 	
 	private final String type;
+	private final TileEnum startingTileName;
 	
 	/**
      * constructor for Adventurer taking in the type associated 
      * with the Adventurer that will be printed on the board.
      * @param str String which will be set to the type of the adventurer.
      */
-	private AdventurerEnum(String str) {
+	private AdventurerEnum(String str, TileEnum startingTileName) {
 		type = str; 
+		this.startingTileName = startingTileName;
     }
 	
 	
@@ -36,6 +38,15 @@ public enum AdventurerEnum {
 	@Override
 	public String toString() {
 		return this.type;
+	}
+	
+	/**
+     * gets starting tile of the adventurer 
+     * when the game begins
+     * @return the Tile name of the starting tile.
+     */
+	public TileEnum getStartingTileName() {
+		return this.startingTileName;
 	}
 	
 	
