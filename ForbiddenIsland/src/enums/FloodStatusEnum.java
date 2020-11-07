137d935b3412e723f2dc06260be0f192b7d20459
Demi Oke <demioke98@gmail.com>
2020-11-07 11:27:37 +0000

    finished board get tile methods for player movement. added integer values for floodStatus enum. Minor edits in Player as per Daniel's suggestion. minor tweeks in Tile code layout.

--- ForbiddenIsland/src/enums/FloodStatusEnum.java
+++ ForbiddenIsland/src/enums/FloodStatusEnum.java
@@ -10,28 +10,40 @@ package enums;
 
 public enum FloodStatusEnum {
 	
-	NOT_FLOODED ("Not flodded"),
-	FLOODED     ("Flooded"),
-	SUNKEN      ("Sunken");
+	NOT_FLOODED ("Not flodded", 2),
+	FLOODED     ("Flooded",     1),
+	SUNKEN      ("Sunken",      0);
 	
 	private String str;
+	private int statusLevel;
 	
 	/**
      * constructor for Flood Status taking in the string constant 
      * associated with the status.
      * @param str String which will be the string version of the enum.
      */
-	private FloodStatusEnum(String str) {
+	private FloodStatusEnum(String str, int statusLevel) {
 		this.str = str; 
+		this.statusLevel = statusLevel;
     }
 	
 	/**
-     * gets type relating to type of adventurer
+     * gets string representation of the 
+     * floodStatus
      * @return the name.
      */
 	@Override
 	public String toString() {
 		return this.str;
+	}
+	
+	/**
+     * gets type integer representation of the 
+     * corresponding floodStatus
+     * @return floodStatus as int.
+     */
+	public int toInt() {
+		return this.statusLevel;
 	}
 	
 	
