a0c2576fd3a80f2915306ba726410c806b336407
Demi Oke <demioke98@gmail.com>
2020-12-14 01:48:13 +0000

    added driver Controller class

--- ForbiddenIsland/src/fi/playerController/DiverController.java
+++ ForbiddenIsland/src/fi/playerController/DiverController.java
@@ -1,10 +1,31 @@
 package fi.playerController;
 
+import java.util.ArrayList;
+
+import fi.board.Board;
+import fi.enums.TileEnum;
 import fi.players.Player;
 
 public class DiverController extends PlayerController {
 
 	public DiverController(Player player) {
 		super(player);
+	}
+	
+	@Override
+	private Boolean moveFromSunk(Player sunkPlayer) {
+		TileEnum chosenTile;
+		ArrayList<TileEnum> tilesPlayerCanMoveTo = new ArrayList<TileEnum>();
+		Board board = Board.getInstance();
+		
+		if(tilesPlayerCanMoveTo.isEmpty()) {
+			playerView.playerCantMove();
+			return false;
+		}
+		
+		chosenTile = playerView.tileToMoveFromSunk(sunkPlayer, tilesPlayerCanMoveTo);
+		sunkPlayer.move(chosenTile);
+		playerView.playerHasMovedFromSunk(sunkPlayer);
+		return true;
 	}
 }
