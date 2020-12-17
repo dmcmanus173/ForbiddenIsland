a40067931b02a6d8f132860307f12dbf0a202006
Demi Oke <demioke98@gmail.com>
2020-12-17 12:53:11 +0000

    added claim treasure test to treasure manager unit test.

--- ForbiddenIsland/src/ut/treasures/TreasureManagerTest.java
+++ ForbiddenIsland/src/ut/treasures/TreasureManagerTest.java
@@ -8,7 +8,10 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import fi.board.TreasureTile;
 import fi.cards.Hand;
+import fi.cards.TreasureCard;
+import fi.enums.TileEnum;
 import fi.enums.TreasureEnum;
 import fi.treasures.TreasureManager;
 
@@ -55,6 +58,33 @@ public class TreasureManagerTest {
 			assertTrue("There should only be no collected treasures once treasure manager is declared.", treasureManager.didClaimTreasure(treasureType) == false);
 		}
 	}
+	
+	@Test
+	public void claimedTreasuresWithEnoughTreasureCardsTest() {
+		//TODO: WILL WE NEED TO ADD SAFEGUARD FOR WHEN TREASURE IS BEING CLAIMED BUT HASNT CALLED CANClaim method before hand. if not might change the name of the test.
+		int numOfTreasureCardsOfGivenTreasureType = 4;
+		for(TreasureEnum treasureType: TreasureEnum.values()) {
+			Hand hand = createHandWith(treasureType, numOfTreasureCardsOfGivenTreasureType);
+			treasureManager.claimTreasure(hand, treasureType);
+			assertEquals("The treasure should be claimed once hand has enough treasure cards.", Boolean.TRUE, treasureManager.didClaimTreasure(treasureType));
+			assertEquals("TreasureManager shouold remove the treasure cards from hand.", Boolean.TRUE, hand.getCardsForTreasure(treasureType).isEmpty());
+		}
+		
+		assertEquals("If All Ttreasures have been claimed Treasure manager should state this.", Boolean.TRUE, treasureManager.didClaimAllTreasures());
+		
+	}
+	
+	
+	
+	public Hand createHandWith(TreasureEnum treasureType, int numCards) {
+		 Hand hand = new Hand();
+		 for(int i = 0; i < numCards; i++) {
+			 hand.addCard(new TreasureCard(treasureType));
+		 }
+		return hand;
+	}
+	
+	
 	
 	
 
